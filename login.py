38ab20c361bd798c1ce3446a18fd569974e71916
linh <linh.ha.nguyen20@gmail.com>
2023-07-24 16:39:11 +0700

    add user

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ curl -X 'POST' \
 """
 from fastapi import FastAPI, Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from fastapi_login import LoginManager
@@ -19,7 +19,10 @@ app = FastAPI()
 manager = LoginManager(SECRET, token_url='/auth/token')
 templates = Jinja2Templates(directory="templates")
 
-fake_db = {'johndoe': {'password': 'secret'}}
+fake_db = {
+    'johndoe': {'password': 'secret'},
+    'alice': {'password': 'secret2'}
+}
 
 @manager.user_loader()
 def load_user(username: str):  # could also be an asynchronous function
@@ -48,10 +51,21 @@ def login(data: OAuth2PasswordRequestForm = Depends()):
     access_token = manager.create_access_token(
         data=dict(sub=username)
     )
-    return {'access_token': access_token, 'token_type': 'bearer'}
+    return RedirectResponse(
+        url='/user', 
+        status_code=302, 
+        headers={'Authorization': f"Bearer {access_token}"},
+    )
+    # print(vars(manager))
+    # return {'access_token': access_token}
 
 
 @app.get("/user")
-async def helloworld(user=Depends(manager)):
+async def get_users(user=Depends(manager)):
+    return {'user': user}
+
+
+@app.get("/helloworld")
+async def helloworld(request: Request):
     """Get all the members of a project."""
-    return JSONResponse(content=user)
+    return JSONResponse(content={"members": "HELLO WORLD"})
