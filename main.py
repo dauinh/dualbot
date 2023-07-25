186316cb0e4880b3bbf7d502bac46241adb22acc
linh <linh.ha.nguyen20@gmail.com>
2023-07-25 11:25:13 +0700

    to prod

--- main.py
+++ main.py
@@ -4,29 +4,59 @@ import mimetypes
 mimetypes.add_type("application/javascript", ".js")
 mimetypes.add_type("text/css", ".css")
 
-from fastapi import Request
-from fastapi.responses import (
-    HTMLResponse,
-    RedirectResponse,
-)
-
 import os
 import webbrowser
+from pathlib import Path
+
+
+from contextlib import asynccontextmanager
+from watchfiles import awatch
+
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import (
+    HTMLResponse,
+    JSONResponse,
+    RedirectResponse,
+    FileResponse,
+    PlainTextResponse,
+)
+from fastapi_socketio import SocketManager
+from starlette.middleware.cors import CORSMiddleware
+import asyncio
+
+from chainlit.config import config, load_module, reload_config, DEFAULT_HOST
+from chainlit.client.utils import (
+    get_auth_client_from_request,
+    get_db_client_from_request,
+)
+from chainlit.markdown import get_markdown_str
+from chainlit.telemetry import trace_event
+from chainlit.logger import logger
+from chainlit.types import (
+    CompletionRequest,
+    UpdateFeedbackRequest,
+    GetConversationsRequest,
+    DeleteConversationRequest,
+)
 import requests
+import jwt
 import chainlit as cl
 from chainlit.user_session import user_sessions
 import uuid
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-REDIRECT_URL = os.environ["REDIRECT_URL"]
-OIDC_CLIENT_ID = os.environ["OIDC_CLIENT_ID"]
-OIDC_CLIENT_SECRET = os.environ["OIDC_CLIENT_SECRET"]
-LOGIN_URL = os.environ["LOGIN_URL"]
+REDIRECT_URL = os.environ.get("REDIRECT_URL")
+OIDC_CLIENT_ID = os.environ.get("OIDC_CLIENT_ID")
+OIDC_CLIENT_SECRET = os.environ.get("OIDC_CLIENT_SECRET")
+LOGIN_URL=os.environ.get("LOGIN_URL")
 
+chainlit_route = app.router.routes
+wildcard_route = chainlit_route.pop()
 
+from chainlit.server import html_template
+print(html_template)
 @app.get("/")
 async def serve(request: Request):
     html_template = get_html_template()
@@ -43,9 +73,8 @@ async def serve(request: Request):
     user_sessions[chainlit_session_id] = {"auth_email": auth_email}
     return response
 
-
-@app.get("/auth")
-async def auth(request: Request):
+@app.get("/helloworld")
+async def helloworld(request: Request):
     # print(request._query_params)
     auth_code = request._query_params['code']
     url = 'https://pressingly-account.onrender.com/oauth/token'
@@ -56,31 +85,31 @@ async def auth(request: Request):
         'redirect_uri': REDIRECT_URL,
         'code': auth_code
     }
-    response = requests.post(url, json = myobj).json()
+    public_key = "v5Ins_85RHfOXvMMJ1Peqdjyv-o98CmONMhAtoV0ctI"
+    print("query params", myobj)
+    x = requests.post(url, json = myobj)
+    response = x.json()
+    print(x.json())
+    id_token = response['id_token']
     access_token = 'Bearer ' + response['access_token']
-    headers = {'Authorization': access_token}
-
+    # print(access_token)
     userinfo_url = "https://pressingly-account.onrender.com/oauth/userinfo"
-    y = requests.get(userinfo_url, headers=headers).json()
-    auth_email = y['email']
+    headers = {'Authorization': access_token}
+    y = requests.get(userinfo_url, headers=headers)
+    auth_email = y.json()['email']
+    header, payload, signature = id_token.split('.')
+    decoded_payload = jwt.utils.base64url_decode(payload)
+    decoded_payload = decoded_payload.decode('utf8').replace("'", '"')
 
     response = RedirectResponse("/")
+    # response = JSONResponse(content={"hello": response, "payload": decoded_payload, "userinfo": y.json()})
     response.set_cookie(key="auth_email", value=auth_email)
     return response
 
-
-chainlit_route = app.router.routes
-root_route = chainlit_route[-1]
-chainlit_route.insert(-4, root_route)
-chainlit_route.pop()
-
-hello_route = chainlit_route[-1]
-chainlit_route.insert(-4, hello_route)
-chainlit_route.pop()
+chainlit_route.append(wildcard_route)
 
 # for route in app.router.routes:
 #     print(route)
-
 
 from setup import search_agent
 from utils import create_pdf_agent, process_response
@@ -104,7 +133,7 @@ async def start():
 
     email = cl.user_session.get('auth_email')
     if not email:
-        email = 'Stranger!'
+        email = 'New User!'
     # print("auth_email", email)
     await cl.Message(
         content=f"Hello {email}"+"\nPress this button to switch to chat mode with PDF reader. Open a new chat to reset mode.\nOtherwise, continue to chat for search mode.",
